638dbf027ea0fa656a4930ff3ca2e9c543a6e1ea
vivek <chimnanivivek14@gmail.com>
2023-12-22 11:40:40 +0530

    Changed the frontend's link in server code

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,230 +1,271 @@
-const User = require("../models/User")
-const EmailVerificationToken = require("../models/EmailVerificationToken")
-const PasswordResetToken = require("../models/PasswordResetToken")
-const crypto = require("crypto")
-const jwt = require("jsonwebtoken")
-const { validationResult } = require("express-validator")
-const sgMail = require("@sendgrid/mail")
+const User = require("../models/User");
+const EmailVerificationToken = require("../models/EmailVerificationToken");
+const PasswordResetToken = require("../models/PasswordResetToken");
+const crypto = require("crypto");
+const jwt = require("jsonwebtoken");
+const { validationResult } = require("express-validator");
+const sgMail = require("@sendgrid/mail");
 
 exports.registerUser = async (req, res) => {
-    try {
-        let error = validationResult(req)
-        if (!error.isEmpty()) {
-            return res.status(401).json(error.array()[0].msg)
-        }
-        const { email } = req.body;
-        const isUser = await User.findOne({ email })
-        if (isUser) {
-            return res.status(400).json("User with that email address already exists.")
-        }
-        const user = await User.create({ ...req.body })
-
-        let OTP = ""
-        for (let i = 0; i <= 5; i++) {
-            const randomVal = Math.round(Math.random() * 9)
-            OTP += randomVal;
-        }
-
-        await EmailVerificationToken.create({
-            user: user._id,
-            token: OTP
-        })
-
-        sgMail.setApiKey(process.env.SEND_GRID_API_KEY)
-
-        const msg = {
-            from: "chimnanivivek14@gmail.com",
-            to: user.email,
-            subject: "Email Verification",
-            html: `Your verification OTP is ${OTP}`
-        }
-
-        await sgMail.send(msg)
-
-        return res.status(201).json({ id: user._id, name: user.name, email: user.email, role: user.role })
-    } catch (error) {
-        return res.status(500).json("Something went wrong, please try again.")
+  try {
+    let error = validationResult(req);
+    if (!error.isEmpty()) {
+      return res.status(401).json(error.array()[0].msg);
     }
-}
+    const { email } = req.body;
+    const isUser = await User.findOne({ email });
+    if (isUser) {
+      return res
+        .status(400)
+        .json("User with that email address already exists.");
+    }
+    const user = await User.create({ ...req.body });
+
+    let OTP = "";
+    for (let i = 0; i <= 5; i++) {
+      const randomVal = Math.round(Math.random() * 9);
+      OTP += randomVal;
+    }
+
+    await EmailVerificationToken.create({
+      user: user._id,
+      token: OTP,
+    });
+
+    sgMail.setApiKey(process.env.SEND_GRID_API_KEY);
+
+    const msg = {
+      from: "chimnanivivek14@gmail.com",
+      to: user.email,
+      subject: "Email Verification",
+      html: `Your verification OTP is ${OTP}`,
+    };
+
+    await sgMail.send(msg);
+
+    return res
+      .status(201)
+      .json({
+        id: user._id,
+        name: user.name,
+        email: user.email,
+        role: user.role,
+      });
+  } catch (error) {
+    return res.status(500).json("Something went wrong, please try again.");
+  }
+};
 
 exports.verifyEmail = async (req, res) => {
-    try {
-        const { userId, OTP } = req.body;
-        const user = await User.findById(userId)
-        if (!user) {
-            return res.status(400).json("Invalid user.")
-        }
-        if (user.isVerified) {
-            return res.status(400).json("User already verified.")
-        }
-
-        const token = await EmailVerificationToken.findOne({ user: userId })
-        if (!token) {
-            return res.status(400).json("User not found.")
-        }
-        const isMatched = await token.compareOTP(OTP)
-        if (!isMatched) return res.status(400).json("Enter a valid OTP, please.")
-
-        sgMail.setApiKey(process.env.SEND_GRID_API_KEY)
-
-        const msg = {
-            from: "chimnanivivek14@gmail.com",
-            to: user.email,
-            subject: "Welcome",
-            html: "Welcome to our app, and thanks for choosing us."
-        }
-
-        await sgMail.send(msg)
-        user.isVerified = true
-        await user.save()
-        await EmailVerificationToken.findOneAndDelete({ user: userId })
-        const accessToken = jwt.sign({ id: user._id }, process.env.JWT_SECRET_KEY)
-        return res.json({ name: user.name, email: user.email, id: user._id, accessToken, isVerified: user.isVerified, role: user.role })
-    } catch (error) {
-        return res.status(500).json("Something went wrong, please try again.")
+  try {
+    const { userId, OTP } = req.body;
+    const user = await User.findById(userId);
+    if (!user) {
+      return res.status(400).json("Invalid user.");
     }
-}
+    if (user.isVerified) {
+      return res.status(400).json("User already verified.");
+    }
+
+    const token = await EmailVerificationToken.findOne({ user: userId });
+    if (!token) {
+      return res.status(400).json("User not found.");
+    }
+    const isMatched = await token.compareOTP(OTP);
+    if (!isMatched) return res.status(400).json("Enter a valid OTP, please.");
+
+    sgMail.setApiKey(process.env.SEND_GRID_API_KEY);
+
+    const msg = {
+      from: "chimnanivivek14@gmail.com",
+      to: user.email,
+      subject: "Welcome",
+      html: "Welcome to our app, and thanks for choosing us.",
+    };
+
+    await sgMail.send(msg);
+    user.isVerified = true;
+    await user.save();
+    await EmailVerificationToken.findOneAndDelete({ user: userId });
+    const accessToken = jwt.sign({ id: user._id }, process.env.JWT_SECRET_KEY);
+    return res.json({
+      name: user.name,
+      email: user.email,
+      id: user._id,
+      accessToken,
+      isVerified: user.isVerified,
+      role: user.role,
+    });
+  } catch (error) {
+    return res.status(500).json("Something went wrong, please try again.");
+  }
+};
 
 exports.resendOTP = async (req, res) => {
-    try {
-        const { userId } = req.body;
-        const user = await User.findById(userId)
-        if (!user) {
-            return res.status(400).json("Invalid user.")
-        }
-
-        if (user.isVerified) {
-            return res.status(400).json("User already verified.")
-        }
-
-        const token = await EmailVerificationToken.findOne({ user: userId })
-        if (token) {
-            return res.status(400).json("You can not ask for new OTP within just 1 hour of sending an OTP.")
-        }
-
-        let OTP = ""
-        for (let i = 0; i <= 5; i++) {
-            const randomVal = Math.round(Math.random() * 9)
-            OTP += randomVal;
-        }
-
-        await EmailVerificationToken.create({
-            user: user._id,
-            token: OTP
-        })
-
-        sgMail.setApiKey(process.env.SEND_GRID_API_KEY)
-
-        const msg = {
-            from: "chimnanivivek14@gmail.com",
-            to: user.email,
-            subject: "Email Verification",
-            html: `Your verification OTP is ${OTP}`
-        }
-
-        await sgMail.send(msg)
-
-        return res.status(201).json("A new OTP successfully sent to your email address.")
-
-    } catch (error) {
-        return res.status(500).json("Something went wrong, please try again.")
+  try {
+    const { userId } = req.body;
+    const user = await User.findById(userId);
+    if (!user) {
+      return res.status(400).json("Invalid user.");
     }
-}
+
+    if (user.isVerified) {
+      return res.status(400).json("User already verified.");
+    }
+
+    const token = await EmailVerificationToken.findOne({ user: userId });
+    if (token) {
+      return res
+        .status(400)
+        .json(
+          "You can not ask for new OTP within just 1 hour of sending an OTP."
+        );
+    }
+
+    let OTP = "";
+    for (let i = 0; i <= 5; i++) {
+      const randomVal = Math.round(Math.random() * 9);
+      OTP += randomVal;
+    }
+
+    await EmailVerificationToken.create({
+      user: user._id,
+      token: OTP,
+    });
+
+    sgMail.setApiKey(process.env.SEND_GRID_API_KEY);
+
+    const msg = {
+      from: "chimnanivivek14@gmail.com",
+      to: user.email,
+      subject: "Email Verification",
+      html: `Your verification OTP is ${OTP}`,
+    };
+
+    await sgMail.send(msg);
+
+    return res
+      .status(201)
+      .json("A new OTP successfully sent to your email address.");
+  } catch (error) {
+    return res.status(500).json("Something went wrong, please try again.");
+  }
+};
 
 exports.forgotPassword = async (req, res) => {
-    try {
-        const { email } = req.body;
-        const user = await User.findOne({ email })
-        if (!user) {
-            return res.status(404).json("User not found!")
-        }
-        const token = await PasswordResetToken.findOne({ user: user._id })
-        if (token) {
-            return res.status(400).json("You can not ask us to send new email within just 1 hour of sending an email.")
-        }
-
-        const passToken = new Promise((resolve, reject) => {
-            crypto.randomBytes(30, (err, buff) => {
-                if (err) reject(err)
-                const buffString = buff.toString("hex")
-
-                resolve(buffString)
-            })
-        })
-        const newToken = await passToken;
-        await PasswordResetToken.create({ user: user._id, token: newToken })
-        const passwordResetUrl = `http://127.0.0.1:5173/auth/reset-password?token=${newToken}&id=${user._id}`
-
-        sgMail.setApiKey(process.env.SEND_GRID_API_KEY)
-
-        const msg = {
-            from: "chimnanivivek14@gmail.com",
-            to: user.email,
-            subject: "Forgot Password",
-            html: `If you requested to reset your password, <a href=${passwordResetUrl}>Click this link</a>`
-        }
-
-        await sgMail.send(msg)
-
-        return res.status(201).json("Check Your Mailbox.")
-
-    } catch (error) {
-        return res.status(500).json("Something went wrong, please try again.")
+  try {
+    const { email } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(404).json("User not found!");
     }
-}
+    const token = await PasswordResetToken.findOne({ user: user._id });
+    if (token) {
+      return res
+        .status(400)
+        .json(
+          "You can not ask us to send new email within just 1 hour of sending an email."
+        );
+    }
+
+    const passToken = new Promise((resolve, reject) => {
+      crypto.randomBytes(30, (err, buff) => {
+        if (err) reject(err);
+        const buffString = buff.toString("hex");
+
+        resolve(buffString);
+      });
+    });
+    const newToken = await passToken;
+    await PasswordResetToken.create({ user: user._id, token: newToken });
+    const passwordResetUrl = `http://localhost:5173/auth/reset-password?token=${newToken}&id=${user._id}`;
+
+    sgMail.setApiKey(process.env.SEND_GRID_API_KEY);
+
+    const msg = {
+      from: "chimnanivivek14@gmail.com",
+      to: user.email,
+      subject: "Forgot Password",
+      html: `If you requested to reset your password, <a href=${passwordResetUrl}>Click this link</a>`,
+    };
+
+    await sgMail.send(msg);
+
+    return res.status(201).json("Check Your Mailbox.");
+  } catch (error) {
+    return res.status(500).json("Something went wrong, please try again.");
+  }
+};
 
 exports.resetPassword = async (req, res) => {
-    try {
-        const { userId, token, password } = req.body;
-        const user = await User.findById(userId)
-        if (!user) return res.status(404).json("User not found!")
-        const resetToken = await PasswordResetToken.findOne({ user: userId })
-        if (!resetToken) return res.status(404).json("Token expired, try resetting your password again.")
-        const isMatched = await resetToken.compareToken(token)
-        if (!isMatched) return res.status(404).json("Invalid request.")
+  try {
+    const { userId, token, password } = req.body;
+    const user = await User.findById(userId);
+    if (!user) return res.status(404).json("User not found!");
+    const resetToken = await PasswordResetToken.findOne({ user: userId });
+    if (!resetToken)
+      return res
+        .status(404)
+        .json("Token expired, try resetting your password again.");
+    const isMatched = await resetToken.compareToken(token);
+    if (!isMatched) return res.status(404).json("Invalid request.");
 
-        const isOldPassword = await user.comparePassword(password)
-        if (isOldPassword) return res.status(400).json("Please provide a new password.")
-        user.password = password;
-        await user.save()
-        await PasswordResetToken.findByIdAndDelete(resetToken._id)
+    const isOldPassword = await user.comparePassword(password);
+    if (isOldPassword)
+      return res.status(400).json("Please provide a new password.");
+    user.password = password;
+    await user.save();
+    await PasswordResetToken.findByIdAndDelete(resetToken._id);
 
-        sgMail.setApiKey(process.env.SEND_GRID_API_KEY)
+    sgMail.setApiKey(process.env.SEND_GRID_API_KEY);
 
-        const msg = {
-            from: "chimnanivivek14@gmail.com",
-            to: user.email,
-            subject: "Password Reset Successful!",
-            html: "You Reset your Password Successfully!"
-        }
+    const msg = {
+      from: "chimnanivivek14@gmail.com",
+      to: user.email,
+      subject: "Password Reset Successful!",
+      html: "You Reset your Password Successfully!",
+    };
 
-        await sgMail.send(msg)
+    await sgMail.send(msg);
 
-        return res.status(201).json("You successfully changed your password.")
-    } catch (error) {
-        return res.status(500).json("Something went wrong, please try again.")
-    }
-}
+    return res.status(201).json("You successfully changed your password.");
+  } catch (error) {
+    return res.status(500).json("Something went wrong, please try again.");
+  }
+};
 
 exports.loginUser = async (req, res) => {
-    try {
-        const { email, password } = req.body;
-        const user = await User.findOne({ email })
-        if (!user) return res.status(401).json("Invalid email or password.")
-        const isPassword = await user.comparePassword(password)
-        if (!isPassword) return res.status(401).json("Invalid email or password.")
-        const accessToken = jwt.sign({ id: user._id }, process.env.JWT_SECRET_KEY)
-        return res.json({ name: user.name, email, id: user._id, accessToken, isVerified: user.isVerified, role: user.role })
-    } catch (error) {
-        return res.status(500).json("Something went wrong, please try again.")
-    }
-}
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) return res.status(401).json("Invalid email or password.");
+    const isPassword = await user.comparePassword(password);
+    if (!isPassword) return res.status(401).json("Invalid email or password.");
+    const accessToken = jwt.sign({ id: user._id }, process.env.JWT_SECRET_KEY);
+    return res.json({
+      name: user.name,
+      email,
+      id: user._id,
+      accessToken,
+      isVerified: user.isVerified,
+      role: user.role,
+    });
+  } catch (error) {
+    return res.status(500).json("Something went wrong, please try again.");
+  }
+};
 
 exports.isAuth = (req, res) => {
-    const { user } = req;
-    try {
-        return res.json({ id: user._id, name: user.name, email: user.email, isVerified: user.isVerified, role: user.role })
-    } catch (error) {
-        return res.status(500).json("Something went wrong, please try again.")
-    }
-}
+  const { user } = req;
+  try {
+    return res.json({
+      id: user._id,
+      name: user.name,
+      email: user.email,
+      isVerified: user.isVerified,
+      role: user.role,
+    });
+  } catch (error) {
+    return res.status(500).json("Something went wrong, please try again.");
+  }
+};
