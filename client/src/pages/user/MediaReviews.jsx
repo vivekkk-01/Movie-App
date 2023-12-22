74bbaa29f5ae9f7aa88410f1d084369f0d7763b6
vivek <chimnanivivek14@gmail.com>
2023-12-22 11:34:08 +0530

    showing loader while fetching reveiws

--- client/src/pages/user/MediaReviews.jsx
+++ client/src/pages/user/MediaReviews.jsx
@@ -10,6 +10,7 @@ import EditRatingModal from "../../components/user/EditRatingModal";
 
 const MediaReviews = () => {
   const [reviews, setReviews] = useState([]);
+  const [loading, setLoading] = useState(false);
   const [showConfirmModal, setShowConfirmModal] = useState(false);
   const [showEditModal, setShowEditModal] = useState(false);
   const [profileOwnersReview, setProfileOwnersReview] = useState(null);
@@ -20,8 +21,10 @@ const MediaReviews = () => {
   const { authInfo } = useAuth();
 
   const fetchReviews = async () => {
+    setLoading(true);
     const { type, response } = await getReviews(mediaId);
     if (type === "error") return updateNotification(type, response);
+    setLoading(false);
     setReviews([...response]);
   };
 
@@ -65,15 +68,24 @@ const MediaReviews = () => {
     setReviews([...updatedReviews]);
   };
 
+  if (loading)
+    return (
+      <div className="h-screen flex justify-center items-center dark:bg-primary bg-white">
+        <p className="text-2xl text-light-subtle dark:text-dark-subtle animate-pulse">
+          Please Wait...
+        </p>
+      </div>
+    );
+
   return (
     <div className="dark:bg-primary bg-white min-h-screen pb-10">
       <Container className="w-4/5 mx-auto xs:w-screen px-4 py-8">
-        {reviews?.length <= 0 && (
+        {reviews?.length <= 0 && !loading && (
           <h1 className="font-semibold text-3xl text-secondary dark:text-white text-center opacity-80">
             Reviews Not Found!
           </h1>
         )}
-        {reviews?.length > 0 && (
+        {reviews?.length > 0 && !loading && (
           <div className="flex justify-between items-center">
             <h1 className="text-2xl font-semibold text-secondary dark:text-white">
               <span className="text-light-subtle dark:text-dark-subtle font-normal">
